96ee46a7c26e4450f2d744ff4a2bc74cbb1e2124
Marri <amarri@paypal.com>
2020-05-19 09:52:50 -0700

    fixing lint format issue

--- server/remember.js
+++ server/remember.js
@@ -9,7 +9,7 @@ import { getSDKCookie, writeSDKCookie, type CookiesType } from './cookie';
 import { getNonce, getQuery, buildCSP, getTimestamp, isIE } from './util';
 import { COOKIE_SETTINGS } from './config';
 
-export function isFundingRemembered(req : ExpressRequest, fundingSource : $Values<typeof FUNDING>, opts? : { cookies? : CookiesType } = {}) : boolean {
+export function isFundingRemembered(req : ExpressRequest, fundingSource : $Values<typeof FUNDING>, opts? : {| cookies? : CookiesType |} = {}) : boolean {
     const cookies = opts.cookies || req.cookies || {};
     const cookieSettings = COOKIE_SETTINGS[fundingSource] || {};
 
@@ -62,7 +62,7 @@ function parseFundingSources(commaSeparatedFundingSources) : $ReadOnlyArray<$Val
     return commaSeparatedFundingSources.split(',');
 }
 
-function setSecurityHeaders(req : ExpressRequest, res : ExpressResponse, { nonce, domain } : { nonce : string, domain : string }) {
+function setSecurityHeaders(req : ExpressRequest, res : ExpressResponse, { nonce, domain } : {| nonce : string, domain : string |}) {
     const cspHeader = isIE(req)
         ? HTTP_RESPONSE_HEADER.X_CONTENT_SECURITY_POLICY
         : HTTP_RESPONSE_HEADER.CONTENT_SECURITY_POLICY;
