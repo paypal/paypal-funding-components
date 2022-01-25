04fafed6030dfc34f696d3fa6cd30d42738b920c
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2022-01-25 16:44:39 -0600

    chore: add missing globals for testing

--- test/client/test-globals.js
+++ test/client/test-globals.js
@@ -8,6 +8,9 @@ export const testGlobals = {
     __SDK_HOST__:   'test.paypal.com',
     __PATH__:       '/sdk/js',
 
+    __PAYPAL_DOMAIN__:     'mock://www.paypal.com',
+    __PAYPAL_API_DOMAIN__: 'mock://msmaster.qa.paypal.com',
+
     __VERSION__:        '1.0.45',
     __CORRELATION_ID__: 'abc123',
     __NAMESPACE__:      'paypal',
