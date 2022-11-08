12b8e8439bcc1ed1ef6ca7d4bf3f6177e2605285
Greg Jopa <534034+gregjopa@users.noreply.github.com>
2022-11-08 10:48:02 -0600

    chore(deps): upgrade grumbler-scripts to version 8.0.1 (#45)

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /* @flow */
 /* eslint import/no-nodejs-modules: off */
 
-import type { WebpackConfig } from '@krakenjs/grumbler-scripts/config/types';
+import type { WebpackConfig } from '@krakenjs/webpack-config-grumbler/index.flow';
 import { getWebpackConfig } from '@krakenjs/grumbler-scripts/config/webpack.config';
 
 import { testGlobals } from './test/client/test-globals';
