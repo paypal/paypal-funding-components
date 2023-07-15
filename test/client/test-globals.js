02b52a0695698f08180b975c6692ef70d1808098
Westin Wrzesinski <westin.wrzesinski@gmail.com>
2023-07-15 15:38:38 -0500

    ci: use sandbox urls

--- test/client/test-globals.js
+++ test/client/test-globals.js
@@ -2,14 +2,14 @@
 
 export const testGlobals = {
     __PORT__:       8000,
-    __STAGE_HOST__: 'msmaster.qa.paypal.com',
+    __STAGE_HOST__: 'sandbox.paypal.com',
     __HOST__:       'test.paypal.com',
     __HOSTNAME__:   'test.paypal.com',
     __SDK_HOST__:   'test.paypal.com',
     __PATH__:       '/sdk/js',
 
     __PAYPAL_DOMAIN__:     'mock://www.paypal.com',
-    __PAYPAL_API_DOMAIN__: 'mock://msmaster.qa.paypal.com',
+  __PAYPAL_API_DOMAIN__: 'mock://sandbox.paypal.com',
 
     __VERSION__:        '1.0.45',
     __CORRELATION_ID__: 'abc123',
